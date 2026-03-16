53650adb66863c45d706b3a004818cc8f21dcaf9
Patrick MacDonald <patrick@patmac.ca>
2026-03-16 13:43:03 -0600

    hide guest page from none admin

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,28 +1,41 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
+// 1. Define which routes require a login
 const isProtectedRoute = createRouteMatcher([
   "/tracker(.*)",
   "/settings(.*)",
   "/inventory(.*)",
   "/experimental(.*)",
+  "/guests(.*)",
+]);
+
+const isAdminRoute = createRouteMatcher([
+  "/settings(.*)",
+  "/experimental(.*)",
+  "/guests(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
     const { has } = await auth.protect();
 
-    if (req.nextUrl.pathname.startsWith("/experimental")) {
-      if (!(await has({ role: "org:admin" })) || (await has({ plan: "free" })))
+    if (isAdminRoute(req)) {
+      const isAdmin = await has({ role: "org:admin" });
+
+      const isFreePlan = await has({ plan: "free" });
+      const isExperimental = req.nextUrl.pathname.startsWith("/experimental");
+
+      if (!isAdmin || (isExperimental && isFreePlan)) {
         return Response.redirect(new URL("/", req.url));
+      }
     }
   }
 });
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest|api/webhooks)).*)",
-    // Always run for API routes
-    "/(api|trpc|tracker|settings|inventory|experimental)(.*)",
+    // Standard Next.js/Clerk middleware matcher
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
   ],
 };
