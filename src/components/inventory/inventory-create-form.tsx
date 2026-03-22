d04867a4af769494d04b0d377ead638e4ce5884b
Patrick MacDonald <patrick@patmac.ca>
2026-03-22 16:52:49 -0600

    added tanstack form for inventory create form

--- src/components/inventory/inventory-create-form.tsx
+++ src/components/inventory/inventory-create-form.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { useAuth } from "@clerk/nextjs";
-import { use, useRef, useState } from "react";
-import { toast } from "sonner";
+import { useForm } from "@tanstack/react-form";
+import { use } from "react";
+import z from "zod";
 import {
   Card,
   CardContent,
@@ -17,18 +18,17 @@ import { Label } from "@/components/ui/label";
 import type { DbResult, UnitType } from "@/db/types";
 import { useAddEquipmentMutation } from "@/mutations/equipment";
 import { Button } from "../ui/button";
-
 export function InventoryCreateForm({
   equipmentTypePromise,
 }: {
   equipmentTypePromise: Promise<DbResult<UnitType[]>>;
 }) {
-  const formRef = useRef<HTMLFormElement>(null);
+  // const formRef = useRef<HTMLFormElement>(null);
   const { has } = useAuth();
   const isAdmin = has({ role: "org:admin" });
   const { mutate: addEquipment, isPending } = useAddEquipmentMutation();
 
-  const [selectedType, setSelectedType] = useState<string>("Raft");
+  // const [selectedType, setSelectedType] = useState<string>("Raft");
 
   const data = use(equipmentTypePromise);
 
@@ -39,22 +39,43 @@ export function InventoryCreateForm({
     value: t.name,
   }));
 
-  if (!isAdmin) return null;
-  const handleAdd = async (formData: FormData) => {
-    const type = selectedType;
-    const unit_number = formData.get("unit_number") as string;
+  const createInventorySchema = z.object({
+    unit_number: z.string().min(1, "Name too short").max(50, "Name too long"),
+    type: z.string().min(1, "Pick or create a type").max(50, "Type too long"),
+  });
 
-    addEquipment(
-      { type, unit_number },
-      {
-        onSuccess: () => {
-          toast.success(`Added equipment ${unit_number}`);
-          formRef.current?.reset();
-        },
-        onError: (error: Error) => toast.error(error.message),
-      },
-    );
-  };
+  const { Field, handleSubmit } = useForm({
+    defaultValues: {
+      unit_number: "",
+      type: "",
+    },
+    onSubmit: async ({ value }) => {
+      console.log(value);
+    },
+    validators: {
+      onSubmit: createInventorySchema,
+      onBlur: createInventorySchema,
+      // onChange: createInventorySchema,
+    },
+  });
+
+  if (!isAdmin) return null;
+
+  // const handleAdd = async (formData: FormData) => {
+  //   const type = selectedType;
+  //   const unit_number = formData.get("unit_number") as string;
+
+  //   addEquipment(
+  //     { type, unit_number },
+  //     {
+  //       onSuccess: () => {
+  //         toast.success(`Added equipment ${unit_number}`);
+  //         // formRef.current?.reset();
+  //       },
+  //       onError: (error: Error) => toast.error(error.message),
+  //     },
+  //   );
+  // };
 
   return (
     // <div className="grid md:grid-cols-3 gap-8">
@@ -64,26 +85,64 @@ export function InventoryCreateForm({
           <CardTitle>Add Equipment</CardTitle>
           <CardDescription>Standard unit into the DB.</CardDescription>
         </CardHeader>
-        <form ref={formRef} action={handleAdd}>
+        <form
+          // ref={formRef}
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit();
+          }}
+        >
           <CardContent className="space-y-4">
             <div className="space-y-2">
               <Label>Unit Number (ID)</Label>
-              <Input
-                name="unit_number"
-                placeholder="e.g. R-402"
-                required
-                disabled={isPending}
-              />
+              <Field name="unit_number">
+                {(field) => {
+                  const { errors, isTouched } = field.state.meta;
+                  return (
+                    <>
+                      <Input
+                        name="unit_number"
+                        placeholder="e.g. R-402"
+                        onBlur={field.handleBlur}
+                        onChange={(e) => field.handleChange(e.target.value)}
+                        disabled={isPending}
+                      />
+                      {ErrorEvent.length > 0 && isTouched && (
+                        <span className="text-red-500">
+                          {errors[0]?.message}
+                        </span>
+                      )}
+                    </>
+                  );
+                }}
+              </Field>
             </div>
+
             <div className="space-y-2">
               <Label>Type</Label>
-              <Combobox
-                options={unitTypeOptions}
-                value={selectedType}
-                onValueChange={setSelectedType}
-                placeholder="Select or Type Type"
-                allowCustom={true}
-              />
+              <Field name="type">
+                {(field) => {
+                  const { errors, isTouched } = field.state.meta;
+                  return (
+                    <>
+                      <Combobox
+                        onBlur={field.handleBlur}
+                        options={unitTypeOptions}
+                        value={field.state.value}
+                        onValueChange={(val) => field.handleChange(val)}
+                        placeholder="Select or Type Type"
+                        allowCustom={true}
+                        disabled={isPending}
+                      />
+                      {ErrorEvent.length > 0 && isTouched && (
+                        <span className="text-red-500">
+                          {errors[0]?.message}
+                        </span>
+                      )}
+                    </>
+                  );
+                }}
+              </Field>
             </div>
           </CardContent>
           <CardFooter>
